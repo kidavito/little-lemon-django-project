2c6ec57222c5ac9d6aad125209f2660967ffcfa6
kidavito <kidavito.user@gmail.com>
2023-02-15 22:28:29 +0700

    fix models

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    inventory = models.IntegerField(max_length=5)
+    inventory = models.IntegerField()
 
 class Booking(models.Model):
     name = models.CharField(max_length=255)
-    no_of_guest = models.IntegerField(max_length=6)
-    booking_date = models.DateTimeField
+    no_of_guest = models.IntegerField()
+    booking_date = models.DateTimeField(default=None)
