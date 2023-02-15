5a7f62e257b34fa0654ae2c38f880a11563eb3e6
kidavito <kidavito.user@gmail.com>
2023-02-15 23:15:40 +0700

    serializers added

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('say-hello/', views.sayHello, name='sayHello'),
     path('', views.index, name='index'),
+    path('api/menu/', views.MenuView.as_view()),
+    path('api/booking/', views.BookingView.as_view()),
 ]
