04bf8f1599259dc624d75fcbc32cbe4b88886973
kidavito <kidavito.user@gmail.com>
2023-02-15 15:18:30 +0700

    setting up homepage

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.sayHello, name='sayHello'),
+    path('say-hello/', views.sayHello, name='sayHello'),
+    path('', views.index, name='index'),
 ]
--- Restaurant/views.py
+++ Restaurant/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def sayHello(request):
     return HttpResponse('Hello World')
+
+def index(request):
+    return render(request, 'index.html', {})
