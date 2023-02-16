b7f959bc29b3e08e7be34e7cf80b708e813baf49
kidavito <kidavito.user@gmail.com>
2023-02-16 12:30:17 +0700

    djoser installed

--- LittleLemonDjangoProject/settings.py
+++ LittleLemonDjangoProject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'Restaurant',
     'rest_framework',
     'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DJOSER = {
+    "USER_ID_FIELD":"username"
+}
