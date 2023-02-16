2436c3e88618841d3176fb57d4f0bf5f04c5ed25
kidavito <kidavito.user@gmail.com>
2023-02-16 12:23:17 +0700

    model enhanced

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -8,7 +8,7 @@ class Menu(models.Model):
     # item = models.CharField(max_length=100)
     # price = models.IntegerField()
     def __str__(self):
-        return f"{self.title}"
+        return f"{self.title} : ${str(self.price)}"
 
 class Booking(models.Model):
     name = models.CharField(max_length=255)
