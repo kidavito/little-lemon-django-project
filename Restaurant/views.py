b6335969391d130dfa1b3c8f2132a77471a568c5
kidavito <kidavito.user@gmail.com>
2023-02-17 15:33:44 +0700

    unnecessary code removed

--- Restaurant/views.py
+++ Restaurant/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from rest_framework import generics, viewsets
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Menu, Booking
@@ -20,31 +18,10 @@ def index(request):
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuItemSerializer
-    # def get_permissions(self):
-    #     permission_classes = []
-    #     if self.request.method != 'GET':
-    #         permission_classes = []
-    #     return [permission() for permission in permission_classes]
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuItemSerializer
-    # def get_permissions(self):
-    #     permission_classes = []
-    #     if self.request.method != 'GET':
-    #         permission_classes = [IsAuthenticated]
-    #     return [permission() for permission in permission_classes]
-
-# class BookingView(APIView):
-#     def get(self, request):
-#         items = Booking.objects.all()
-#         serializer = BookingSerializer(items, many=True)
-#         return Response(serializer.data)
-#     def post(self, request):
-#         serializer = BookingSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response({"status": "success", "data": serializer.data})
 
 class BookingViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
